eff1fc844f4fed50142dd25a81bfc5167e4d2480
leenades <57545008+leenades@users.noreply.github.com>
2020-06-11 12:18:51 +0300

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function gotoTop(){
+    document.getElementById("header-section-top").scrollIntoView();
+}
+function gotoSkills() {
+    document.getElementById("skills-section").scrollIntoView();
+}
+
+function gotoProjects() {
+    document.getElementById("projects-section").scrollIntoView();
+}
+function gotoUIUXProjects() {
+    document.getElementById("uiux-projects-section").scrollIntoView();
+}
+function gotoFooter() {
+    document.getElementById("footer-section").scrollIntoView();
+}
